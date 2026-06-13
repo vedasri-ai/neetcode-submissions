e37b08b1f20df22fa88f480072471b07af1824e0
vedasri <vedathequeen@gmail.com>
2026-06-13 21:04:34 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        vector<int> count(26, 0);
+        for (char c : s) count[c - 'a']++;
+        for (char c : t) {
+            if (--count[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+};
